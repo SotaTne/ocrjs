c0a39fff051c9ff34a6d6a7f77656ffa59d22a80
Sota Tsunemine <motarou.engineer@gmail.com>
2026-04-14 14:46:32 +0900

    feat: infra-contractのexportを明示的に変更

--- packages/infra-contract/src/index.ts
+++ packages/infra-contract/src/index.ts
@@ -1,4 +1,15 @@
 export { ErrorableBase } from './base/ErrorableBase';
-export * from './interfaces/primitives/index';
+export type {
+  IContour,
+  IGeometryPolygon,
+  IGeometryPolygonFactory,
+  IImage,
+  IImageFactory,
+  IModel,
+  IModelLoader,
+  ITensor,
+  ITensorFactory,
+} from './interfaces/primitives/index';
+export type { ModelLoadOptions } from './types/CommonTypes';
 export type * from './types/CommonTypes';
 export type * from './types/Errorable';
